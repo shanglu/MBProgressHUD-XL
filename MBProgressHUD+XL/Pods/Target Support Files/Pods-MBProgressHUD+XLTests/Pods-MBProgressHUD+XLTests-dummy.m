d74187db09febb99951e973970fac2aa3dda0857
路 <lu@macbook-pro.local>
2018-05-29 16:01:55 +0800

    pods

---
+++ MBProgressHUD+XL/Pods/Target Support Files/Pods-MBProgressHUD+XLTests/Pods-MBProgressHUD+XLTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MBProgressHUD_XLTests : NSObject
+@end
+@implementation PodsDummy_Pods_MBProgressHUD_XLTests
+@end
---
+++ MBProgressHUD+XL/Pods/Target Support Files/Pods-MBProgressHUD+XLUITests/Pods-MBProgressHUD+XLUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MBProgressHUD_XLUITests : NSObject
+@end
+@implementation PodsDummy_Pods_MBProgressHUD_XLUITests
+@end
